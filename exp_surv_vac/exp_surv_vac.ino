ace66e1ba6d35d8746f33248236b760fe65bb1df
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2020-09-08 16:39:44 +0200

    Modified MAC address

--- exp_surv_vac/exp_surv_vac.ino
+++ exp_surv_vac/exp_surv_vac.ino
@@ -21,7 +21,7 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+  0xA8, 0x61, 0x0A, 0xAE, 0x7D, 0x12 };
 IPAddress ip(192,168,1,177);
 
 // Initialize the Ethernet server library
