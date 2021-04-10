a011bada7238a97fe39ff12412710f34a4f3b754
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-04-10 17:45:03 +0200

    Code cleanup: Port to OOP

--- filter_past_spikes.py
+++ filter_past_spikes.py
@@ -6,84 +6,127 @@ Created on Wed Apr 07 10:00:50 2021
 @author: jp
 
 Filter past spikes in time-stamped dataseries for experiment monitoring with
-Arduino.
+influxdb.
 
-Takes the output from the script unit_conv.py and checks for spikes in the
-measured and converted data. If a spike is detected, the boolean write_to_db
-is set to False.
+Checks the influxdb database for spikes in past data based on a predefined
+intervall for each parameter. All detected spikes can be deleted by setting
+the corresponding flag before execution.
 """
-#%%
-import pickle
+
+# Standard library imports
 import time
 import datetime
 from influxdb import InfluxDBClient
+import multiprocessing
 
-# Define limit for definition of spike in data:
 
+""" USER INPUT """
+
+# Specify databse to use:
+# database = 'helium2'
+
+# Specify limit for definition of spike in data:
 accepted_range = {}
-accepted_range['sc_vac'] = {'lower': 1e-11, 'upper': 4e-9}
-accepted_range['lab_temperature'] = {'lower': 15, 'upper': 30}
+accepted_range['sc_vac'] = {'lower': 1e-11, 'upper': 4e-9}  # mbar, no calib
+accepted_range['lab_temperature'] = {'lower': 15, 'upper': 30}  # deg C
 
-# Connect to database:
-client = InfluxDBClient(host='localhost', port=8086, database='helium2')
 
-# Get list of measurement series:
-series_result = client.query('SHOW series').raw
-series_list = [series_result['series'][0]['values'][i][0].split(',')[0] for i in range(len(series_result['series'][0]['values']))]
+class SpikeFilter:
 
-# Get all data_
-all_data = {}
-for series in series_list:
-    all_data_result = client.query('SELECT * FROM "{}"'.format(series)).raw
-    all_data[series] = [all_data_result['series'][0]['values'][i][-1] for i in range(len(all_data_result['series'][0]['values']))]
+    def __init__(self, database, accepted_range):
+        self.database = database
+        self.client = InfluxDBClient(
+            host='localhost', port=8086, database=self.database
+            )
+        self.accepted_range = accepted_range
+        self.series_list = list(self.accepted_range.keys())
+        self.data = {}
+        self.timestamps = {}
+        self.spike_indices = {}
+        self.spike_timestamps = {}
+        self.spike_utimestamps = {}
+        self.total_spikes = {}
 
-# Find spike indices:
-spike_indices = {}
-for series in all_data.keys():
-    spike_indices[series] = []
-    for i in range(len(all_data[series])-1):
-        if all_data[series][i] < accepted_range[series]['lower'] or all_data[series][i] > accepted_range[series]['upper']:
-            spike_indices[series].append(i)
+    @staticmethod
+    def is_inbounds(data_point, lower_bound, upper_bound, inclusive=True):
+        if inclusive:
+            return True if lower_bound <= data_point <= upper_bound else False
+        else:
+            return True if lower_bound < data_point < upper_bound else False
 
-all_data = {}
-for series in series_list:
-    all_data_result = client.query('SELECT * FROM "{}"'.format(series)).raw
-    all_data[series] = [all_data_result['series'][0]['values'][i][0] for i in range(len(all_data_result['series'][0]['values']))]
-
-spike_times = {}
-
-for series in series_list:
-    spike_times[series] = []
-    for i in range(len(spike_indices[series])):
-        spike_times[series].append(all_data[series][spike_indices[series][i]])
-
-# pickel.dump(spike_times, open("spike_times.p", "wb"))
-
-# spike_times = pickle.load(open("spike_times.p", "rb" ))
-
-spike_utimes = {}
-
-def conv_to_u_time(date_time):
+    @staticmethod
+    def conv_to_u_time(date_time):
         # date_time = date_time + datetime.timedelta(hours=2)
         u_time = time.mktime(date_time.timetuple())
         u_time_str = str(u_time)[:-2] + 9 * '0'
         return u_time_str
 
+    def display_series(self):
+        pass
 
-for series in spike_times.keys():
-    spike_utimes[series] = []
-    for i in range(len(spike_times[series])):
-        spike_time = spike_times[series][i].strip('Z').replace('T', ' ')
-        spike_datetime = datetime.datetime.strptime(spike_time, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=1)  # Before summer time; after: +2h
-        spike_utime = time.mktime(spike_datetime.timetuple())
-        spike_utime_str = str(spike_utime)[:-2] + 9 * '0'
-        spike_utimes[series].append(spike_utime_str)
+    def find_spikes(self, series):
+        # Get data:
+        data_result = self.client.query(
+            'SELECT * FROM "{}"'.format(series)
+            ).raw
+        self.data[series] = [data_result['series'][0]['values'][i][-1] for i in
+                             range(len(data_result['series'][0]['values']))]
+        # Get timestaps:
+        self.timestamps[series] = [data_result['series'][0]['values'][i][0] for
+                                   i in range(len(data_result['series'][0]
+                                                        ['values']))]
+        # Find spike indices:
+        self.spike_indices[series] = []
+        for i in range(len(self.data[series])-1):
+            if not self.is_inbounds(self.data[series][i],
+                                    self.accepted_range[series]['lower'],
+                                    self.accepted_range[series]['upper']):
+                self.spike_indices[series].append(i)
+        # Get timestamps at spike indices:
+        self.spike_timestamps[series] = []
+        for i in range(len(self.spike_indices[series])):
+            self.spike_timestamps[series].append(
+                self.timestamps[series][self.spike_indices[series][i]]
+                )
+        # Convert spike timestamp to unix time:
+        self.spike_utimestamps[series] = []
+        for i in range(len(self.spike_timestamps[series])):
+            spike_timestamp = self.spike_timestamps[series][i].strip('Z')\
+                .replace('T', ' ')
+            spike_datetimestamp = datetime.datetime.strptime(
+                spike_timestamp, '%Y-%m-%d %H:%M:%S') +\
+                datetime.timedelta(hours=1)  # Before summer time; after: +2h
+            spike_utimestamp = time.mktime(spike_datetimestamp.timetuple())
+            spike_utimestamp_str = str(spike_utimestamp)[:-2] + 9 * '0'
+            self.spike_utimestamps[series].append(spike_utimestamp_str)
+        # Get total number of spikes:
+        self.total_spikes[series] = len(self.spike_utimestamps[series])
+        # Delete spikes:
+        for spike in self.spike_utimestamps[series]:
+            # client.query('DELETE FROM "{}" WHERE time = {}'.format(
+            #   series, spike
+            #   ))
+            pass
+        """ print this as table """
+        """ ask delete via user input not flag in script """
+        """ ask limits same """
 
-total_spikes = {}
 
-for series in spike_utimes.keys():
-    total_spikes[series] = len(spike_utimes[series])
-    for spike in spike_utimes[series]:
-        client.query('DELETE FROM "{}" WHERE time = {}'.format(series, spike))
+""" MAIN """
 
-print('Accepted range: {} -> Total spikes: {}: {}; {}: {}'.format(accepted_range, series_list[0], total_spikes[series_list[0]], series_list[1], total_spikes[series_list[1]]))
+
+database = input('Specify database:\n')
+
+begin_time = datetime.datetime.now()
+
+spike_filter = SpikeFilter(database, accepted_range)
+
+with multiprocessing.Pool() as pool:
+    pool.map(spike_filter.find_spikes, spike_filter.series_list)
+
+print(datetime.datetime.now() - begin_time)
+
+# print('Accepted range: {} -> Total spikes: {}: {}; {}: {}'.format(
+#         accepted_range, series_list[0], total_spikes[series_list[0]],
+#         series_list[1], total_spikes[series_list[1]]
+#         ))
