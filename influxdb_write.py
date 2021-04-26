bba91dbfad511b0418a55a98556ae1eec906a311
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-04-26 10:56:20 +0200

    Adapted influxdb_write module to changes in unit_conv module

--- influxdb_write.py
+++ influxdb_write.py
@@ -7,46 +7,56 @@ Created on Thu Nov 19 11:36:28 2020
 
 Python interface for influDB databases.
 
-Takes a list of floats or ints and writes them into a specified inflluxDB
+Takes a list of floats or ints and writes them into a specified influxDB
 database.
 """
 
-from influxdb import InfluxDBClient
-from datetime import datetime
+def influxdb_write():
+    """ Receive measurement data, convert and write into influxDB."""
 
+    # Standard library imports:
+    from influxdb import InfluxDBClient
+    from datetime import datetime
 
-from eth_com import rcv_meas
-from unit_conv import unit_conv
-from filter_spikes import filter_spikes
+    # Local imports:
+    from eth_com import rcv_meas
+    from unit_conv import unit_conv
 
-now = datetime.utcnow() # Grafana assumes UTC
-dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+    # Create timestamp for database:
+    now = datetime.utcnow() # Grafana assumes UTC
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
 
-analog_signals = rcv_meas()
-output_list, write_to_db = unit_conv(analog_signals)
+    # Receive measurements from Arduino:
+    analog_signals = rcv_meas()
 
-if write_to_db:
-    write_to_db = filter_spikes(output_list)
+    # Convert:
+    conv_measurements = unit_conv(analog_signals)
 
-Nport = 8086
+    # Initialize database client:
+    Nport = 8086
+    db_name = 'helium2'
+    client = InfluxDBClient(host='localhost', port=Nport, database=db_name)
 
-client = InfluxDBClient(host='localhost', port=8086, database='helium2')
-
-json_body = []
-for i in range(2):
-    json_body.append(
-        {
-            "measurement": output_list[i]['measurement'],
-            "tags": {
-                "unit": output_list[i]['unit'],
-            },
-            "time": dt_string,
-            "fields": {
-                "value": output_list[i]['value'],
-                "raw": analog_signals[i],
+    # Create JSON:
+    json_body = []
+    for measurement in conv_measurements:
+        json_body.append(
+            {
+                "measurement": measurement['measurement'],
+                "tags": {
+                    "unit": measurement['unit'],
+                },
+                "time": dt_string,
+                "fields": {
+                    "value": measurement['value'],
+                    "raw": measurement['raw'],
+                }
             }
-        }
-    )
+        )
 
-if write_to_db is True:
+    # Write to database:
     client.write_points(json_body)
+
+if __name__ == '__main__':
+
+    influxdb_write()
