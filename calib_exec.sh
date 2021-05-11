1d773d7b5409e5102ed2e559cece18a623c446dc
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-05-11 17:19:34 +0200

    Bug fix in calib_exec.sh

--- calib_exec.sh
+++ calib_exec.sh
@@ -2,8 +2,10 @@
 
 # Update calibration: Pull ->  run calib.py -> push
 
+cd /mnt/md1/exp_monitor
 git pull origin master
 python3 calib.py
 git add calib.p
 git commit -m "Calib update"
 git push origin master
+cd
