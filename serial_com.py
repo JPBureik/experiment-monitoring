73f9eb429b69b5d0e9c868a8875e4a3d5ff7431f
JP Bureik <jp.bureik@protonmail.com>
2021-05-28 22:26:12 +0200

    Layout and documentation

--- serial_com.py
+++ serial_com.py
@@ -4,25 +4,36 @@
 Created on Fri May 28 21:09:53 2021
 
 @author: jp
+
+Serial communication module for receiving measurement values from TPG 261.
+
+Takes the serial port as string and returns the measurement value in a dict
+for JSON to be stored in influxDB.
 """
 
+# Standard library imports:
 import serial  # pip3 install pyserial
+
 
 def tpg261_meas(port):
 
     with serial.Serial(port, baudrate=9600, timeout=1) as ser:
         
+        # Receive measurement bytes from gauge:
         serial_rcv = ser.readline()
-        pressure = float(str(serial_rcv).split(',')[1])
-        
-        # Create dict for JSON:
-        source_vac = {}
-        source_vac['measurement'] = 'source_vac'
-        source_vac['unit'] = 'mbar'
-        source_vac['raw'] = pressure
-        source_vac['value'] = pressure
+    
+    # Convert to float:
+    pressure = float(str(serial_rcv).split(',')[1])
+    
+    # Create dict for JSON:
+    source_vac = {}
+    source_vac['measurement'] = 'source_vac'
+    source_vac['unit'] = 'mbar'
+    source_vac['raw'] = pressure
+    source_vac['value'] = pressure
                 
     return source_vac
+
 
 if __name__ == '__main__':
 
