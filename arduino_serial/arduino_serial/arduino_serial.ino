5a3eb42561e102cbf77390547d3694f0ddfb86c6
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-05-28 15:24:09 +0200

    HEX output over serial

--- arduino_serial/arduino_serial/arduino_serial.ino
+++ arduino_serial/arduino_serial/arduino_serial.ino
@@ -1,15 +1,15 @@
 // Example 6 - Receiving binary data
 
-const byte numBytes = 32;
+const byte numBytes = 64;
 byte receivedBytes[numBytes];
 byte numReceived = 0;
 
 boolean newData = false;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(19200);
     Serial.println("<Arduino is ready>");
-    Serial1.begin(9600);
+    Serial1.begin(19200);
 }
 
 void loop() {
@@ -18,26 +18,25 @@ void loop() {
 }
 
 void recvBytesWithStartEndMarkers() {
-    static boolean recvInProgress = false;
     static byte ndx = 0;
-    byte endMarker = 0x0A;
+    byte endMarker = '\n';//0x0A;
     byte rb;
    
 
     while (Serial1.available() > 0 && newData == false) {
         rb = Serial1.read();
 
-            if (rb != endMarker) {
-                receivedBytes[ndx] = rb;
-                ndx++;
-                if (ndx >= numBytes) {
-                    ndx = numBytes - 1;
-                }
+        if (rb != endMarker) {
+            receivedBytes[ndx] = rb;
+            ndx++;
+            if (ndx >= numBytes) {
+                ndx = numBytes - 1;
+            }
             else {
-                receivedBytes[ndx] = '\0'; // terminate the string
-                numReceived = ndx;  // save the number for use when printing
-                ndx = 0;
-                newData = true;
+              receivedBytes[ndx] = '\0'; // terminate the string
+              numReceived = ndx;  // save the number for use when printing
+              ndx = 0;
+              newData = true;
             }
         }
 
@@ -47,10 +46,11 @@ void recvBytesWithStartEndMarkers() {
 
 void showNewData() {
     if (newData == true) {
+        unsigned char hexValue[2];
         Serial.print("Received HEX values -- ");
         for (byte n = 0; n < numReceived; n++) {
             Serial.print(receivedBytes[n], HEX);
-            Serial.print(' ');
+            Serial.print(' ');         
         }
         Serial.println();
         newData = false;
