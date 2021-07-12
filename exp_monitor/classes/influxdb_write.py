c66c753297dc979abdef1164995756e3f26f9388
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2021-07-12 19:13:10 +0200

    Added Zeeman tc's from master

--- exp_monitor/classes/influxdb_write.py
+++ exp_monitor/classes/influxdb_write.py
@@ -51,6 +51,10 @@ def influxdb_write():
     all_phidgets.append(tc3)
     tc4 = Phidget('Thermocouple', 4, 561242, 3, 'Water')
     all_phidgets.append(tc4)
+    tc5 = Phidget('Thermocouple', 5, 561242, 0, 'Zeeman1')
+    all_phidgets.append(tc5)
+    tc6 = Phidget('Thermocouple', 5, 561242, 1, 'Zeeman2')
+    all_phidgets.append(tc6)
 
     for phidget in all_phidgets:
         temp = phidget.measure()
