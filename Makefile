848e82bb99f41671e6f4fda51fedcce75faebbe6
JP Bureik <jan-philipp.bureik@institutoptique.fr>
2022-02-01 13:48:18 +0100

    Added Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+setup:
+	setup.sh
+
+venv/bin/activate:
+	/home/admin/.local/bin/virtualenv venv
--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bash
+# Shell script to set up the RaspberryPi server for experiment monitoring.
 
 echo "Installing ..."
+# Set system time for InfluxDB time stamps:
 timedatectl set-timezone Europe/Paris
+# Make sure everything is up-to-date:
 apt update && sudo apt -y upgrade
+# To enable X11 forwarding, install the X_Windows package:
 apt install xorg
+# Set the DISPLAY parameter manually:
+export DISPLAY=localhost:10.0
 echo "Done."
