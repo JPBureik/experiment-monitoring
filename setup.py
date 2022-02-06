d4332919567ebaab04a360dbd03d54b83e381d4b
JP Bureik <jp.bureik@protonmail.com>
2022-02-06 13:15:58 +0100

    Revert

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
-import site
-import sys
 
 if __name__ == "__main__":
-    # workaround https://github.com/pypa/pip/issues/7953
-    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
     setup(
     )
