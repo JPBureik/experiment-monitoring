de7478c3ed86c40d8433ef11484b55fb9d95db54
JP Bureik <jp.bureik@protonmail.com>
2022-02-06 13:11:58 +0100

    Pip editable mode install bug

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
+import site
+import sys
 
 if __name__ == "__main__":
+    # workaround https://github.com/pypa/pip/issues/7953
+    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
     setup(
     )
