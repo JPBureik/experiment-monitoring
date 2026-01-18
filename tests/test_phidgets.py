bab947a9636995e793fed3b5301ff08c0c24a76e
JP Bureik <jp.bureik@protonmail.com>
2026-01-18 06:42:26 +0100

    Skip hardware-dependent test in CI, fix package discovery

--- tests/test_phidgets.py
+++ tests/test_phidgets.py
@@ -8,7 +8,15 @@ Testing module for Phidgets class.
 @author: jp
 """
 
+import os
 
+import pytest
+
+
+@pytest.mark.skipif(
+    os.environ.get("CI") == "true",
+    reason="Requires physical Phidget hardware"
+)
 def test_lab_temp_phidget(lab_temp_phidget):
     """Test that Phidget measurement returns correct values."""
     lab_temp_phidget.measure()
