8aae44ec57a8ae01ab7220e5db41f1700a6b1cf2
JP Bureik <jp.bureik@protonmail.com>
2026-01-18 07:13:17 +0100

    Add basic import tests

---
+++ tests/test_import.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Basic import tests to verify package structure."""
+
+
+def test_import_expmonitor():
+    """Test that expmonitor package can be imported."""
+    import expmonitor
+    assert expmonitor is not None
+
+
+def test_import_sensor():
+    """Test that Sensor base class can be imported."""
+    from expmonitor.classes.sensor import Sensor
+    assert Sensor is not None
